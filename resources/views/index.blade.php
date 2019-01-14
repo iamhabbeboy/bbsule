8113f9c88185da15fe89c6dd069b772cc8e0e057
Oluwole <oluwole.majiyagbe@britecore.com>
2019-01-14 09:39:09 +0100

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -432,7 +432,7 @@
 				<section data-id="6c87aab0" class="elementor-element elementor-element-6c87aab0 elementor-section-full_width elementor-section-content-middle elementor-section-height-default elementor-section-height-default elementor-section elementor-top-section" data-element_type="section">
 					<div class="elementor-container elementor-column-gap-default">
 						<div class="elementor-row">
-							<div data-id="26a66581" class="elementor-element elementor-element-26a66581 elementor-column elementor-col-100 elementor-top-column" data-element_type="column">
+							<div data-id="26a66581" class="elementor-element elementor-element-45e1452f elementor-column elementor-col-50 elementor-top-column" data-element_type="column">
 								<div class="elementor-column-wrap elementor-element-populated">
 									<div class="elementor-widget-wrap">
 										<div data-id="7360c3df" class="elementor-element elementor-element-7360c3df elementor-widget elementor-widget-heading" data-element_type="heading.default">
